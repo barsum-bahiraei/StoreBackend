2e914b531d2b4fa877b005557b460cda2b3e3b52
Amin Bahiraei <amin.bahiraei.75@gmail.com>
2025-05-05 13:53:45 +0330

    .

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,13 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using StoreBackend.Data;
-using StoreBackend.Helpers;
+﻿using Microsoft.AspNetCore.Mvc;
 using StoreBackend.Models;
 using StoreBackend.Services;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 
 namespace StoreBackend.Controllers;
 [Route("api/[controller]")]
